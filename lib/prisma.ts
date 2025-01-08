5ca2996f40d50b0da1ed98f3b87796f7f1ebaa5c
Avishek Das <dasavishek2013@gmail.com>
2025-01-08 23:54:05 +0530

    modify prisma file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,38 +1,39 @@
+/* eslint-disable no-var */
 
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+
+
+import { PrismaClient } from '@prisma/client';
+
+// Add type declaration for globalThis in TypeScript
+declare global {
+  // Prevent variable redeclaration error in TypeScript
+  var prisma: PrismaClient | undefined;
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+// Create a single Prisma Client instance
+const prisma = global.prisma || new PrismaClient();
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma; // Ensure the client is reused in development
+}
 
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
-
-
-
-// import { PrismaClient } from '@prisma/client';
-
-// // Add type declaration for globalThis in TypeScript
-// declare global {
-//   // Prevent variable redeclaration error in TypeScript
-//   var prisma: PrismaClient | undefined;
-// }
-
-// // Create a single Prisma Client instance
-// const prisma = global.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== 'production') {
-//   global.prisma = prisma; // Ensure the client is reused in development
-// }
-
-// export default prisma;
+export default prisma;
 
 
 
